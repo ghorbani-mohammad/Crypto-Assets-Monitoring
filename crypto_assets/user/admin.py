7f5c2d025fa5e11420c263715fe60bf8f660931a
Mohammad <aali361@gmail.com>
2022-01-12 02:59:11 +0330

    feat: add ProfileAdmin

--- crypto_assets/user/admin.py
+++ crypto_assets/user/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'mobile_number', 'username']
+    search_fields = ['username']
