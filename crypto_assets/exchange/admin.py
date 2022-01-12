b6accb42ca243798a16fb4f18c97a58f088d4a36
Mohammad <aali361@gmail.com>
2022-01-12 03:32:59 +0330

    feat: add readonlyadmindate fields

--- crypto_assets/exchange/admin.py
+++ crypto_assets/exchange/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 
 from . import models
+from reusable.admins import ReadOnlyAdminDateFields
 
 
 @admin.register(models.Coin)
-class CoinAdmin(admin.ModelAdmin):
+class CoinAdmin(ReadOnlyAdminDateFields, admin.ModelAdmin):
     list_display = ['pk', 'title', 'code']
 
 
 @admin.register(models.Exchange)
-class ExchangeAdmin(admin.ModelAdmin):
+class ExchangeAdmin(ReadOnlyAdminDateFields, admin.ModelAdmin):
     list_display = ['pk', 'name']
