4240dc089c040e09951d27837c6f123570c462e5
Mohammad <aali361@gmail.com>
2022-01-12 04:35:50 +0330

    feat: add log settings

--- crypto_assets/crypto_assets/settings.py
+++ crypto_assets/crypto_assets/settings.py
@@ -107,3 +107,30 @@ DATABASES = {
 }
 
 AUTH_USER_MODEL = 'user.Profile'
+
+
+# LOG
+log_path = "/var/log/app/"
+os.makedirs(os.path.dirname(log_path), exist_ok=True)
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'},
+    },
+    'handlers': {
+        'general_log_file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.WatchedFileHandler',
+            'filename': os.path.join(log_path, 'logs.log'),
+            'formatter': 'standard',
+        },
+    },
+    'loggers': {
+        # all modules
+        '': {
+            'handlers': ['general_log_file'],
+            'level': 'INFO',
+        },
+    },
+}
