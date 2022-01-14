dd5372fdd14751d416fa1b489cb2566ea591ea80
Mohammad Ghorbani <mohammad.ghorbani@hotmail.com>
2022-01-15 02:55:42 +0330

    feat: minor change

--- crypto_assets/exchange/models.py
+++ crypto_assets/exchange/models.py
@@ -72,7 +72,7 @@ class Transaction(BaseModel):
 
     @property
     def get_current_price(self):
-        number = int(self.coin.price(self.market))
+        number = int(int(self.coin.price(self.market)) /10)
         return '{:,}'.format(number)
 
     @property
