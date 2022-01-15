e153ef4adb6eb3fd8fd454b68c85461cb0ae6670
Mohammad Ghorbani <mohammad.ghorbani@hotmail.com>
2022-01-15 18:50:26 +0330

    feat: add coin price caching

--- crypto_assets/exchange/platforms/bitpin.py
+++ crypto_assets/exchange/platforms/bitpin.py
@@ -1,6 +1,8 @@
 import logging
 import decimal
 import requests
+from django.core.cache import cache
+
 from .base import BaseExchange
 
 
@@ -8,14 +10,21 @@ logger = logging.getLogger(__name__)
 
 
 class Bitpin(BaseExchange):
+    def __init__(self):
+        self.api_addr = 'https://api.bitpin.ir/v1/mkt/markets/'
+
     def get_price(self, coin, market):
         market = self.market_mapper(market)
-        api_addr = 'https://api.bitpin.ir/v1/mkt/markets/'
+        coin_key = f'{coin}_{market}'
+        if cache.get(coin_key):
+            return cache.get(coin_key)
         try:
-            coins = requests.get(api_addr).json()['results']
+            coins = requests.get(self.api_addr).json()['results']
             for item in coins:
-                if item['code'] == f'{coin}_{market}':
-                    return round(decimal.Decimal(item['price']), 2)
+                if item['code'] == coin_key:
+                    price = round(decimal.Decimal(item['price']), 2)
+                    cache.set(coin_key, price, 2)
+                    return price
         except Exception as e:
             logger.error(e)
             return None
