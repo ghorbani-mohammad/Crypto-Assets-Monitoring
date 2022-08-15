4b823058aee795f4ca7072b1b98215ae5f33289e
Mohammad <aali361@gmail.com>
2022-08-15 23:47:29 +0430

    add django admin header

--- crypto_assets/crypto_assets/urls.py
+++ crypto_assets/crypto_assets/urls.py
@@ -4,3 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('secret-admin/', admin.site.urls),
 ]
+
+admin.site.index_title = "Crypto Assets"
+admin.site.site_title = "Crypto Assets Django Admin"
+admin.site.site_header = "Crypto Assets Django Administration Panel"
