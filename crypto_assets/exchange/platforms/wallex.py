f988f5db0fb4f720d1d55c49cf51cfff8f77f217
Mohammad <mohammad.ghorbani@hotmail.com>
2022-10-02 07:04:45 +0330

    minor change

--- crypto_assets/exchange/platforms/wallex.py
+++ crypto_assets/exchange/platforms/wallex.py
@@ -9,9 +9,9 @@ logger = logging.getLogger(__name__)
 class Wallex(BaseExchange):
     def get_price(self, coin, market):
         logger.info(f"Wallex.get_price({coin}, {market})")
-        api_addr = 'https://api.wallex.ir/v1/markets'
+        api_addr = "https://api.wallex.ir/v1/markets"
         try:
-            coin = requests.get(api_addr).json()['result']['symbols'][f'{coin}{market}']
-            return round(float(coin['stats']['lastPrice']), 2)
+            coin = requests.get(api_addr).json()["result"]["symbols"][f"{coin}{market}"]
+            return round(float(coin["stats"]["lastPrice"]), 2)
         except:
             return None
