aecdec390d8783bb3e8e2c37cecef19524da26a6
Mohammad <mohammad.ghorbani@hotmail.com>
2022-10-02 06:58:40 +0330

    minor change

--- crypto_assets/crypto_assets/asgi.py
+++ crypto_assets/crypto_assets/asgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crypto_assets.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crypto_assets.settings")
 
 application = get_asgi_application()
