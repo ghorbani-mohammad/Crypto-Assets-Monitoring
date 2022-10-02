e3a5864c4cef4267621ae32ef7cc1486f2072645
Mohammad <mohammad.ghorbani@hotmail.com>
2022-10-02 06:57:45 +0330

    minor change

--- crypto_assets/crypto_assets/celery.py
+++ crypto_assets/crypto_assets/celery.py
@@ -7,22 +7,22 @@ from celery.signals import setup_logging
 from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crypto_assets.settings')
-app = Celery('crypto_assets')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crypto_assets.settings")
+app = Celery("crypto_assets")
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
-app.config_from_object('django.conf:settings')
+app.config_from_object("django.conf:settings")
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
-DEFAULT_QUEUE = 'celery'
+DEFAULT_QUEUE = "celery"
 
 
 if not settings.DEBUG:
 
     @app.task(bind=True)
     def debug_task(self):
-        print('Request: {0!r}'.format(self.request))
+        print("Request: {0!r}".format(self.request))
 
     @setup_logging.connect
     def config_loggers(*args, **kwags):
