62903f8ca101112c3bbfd8777eb8bc732fe53261
Mohammad <mohammad.ghorbani@hotmail.com>
2022-10-06 22:31:56 +0330

    minor change

--- crypto_assets/crypto_assets/urls.py
+++ crypto_assets/crypto_assets/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from django.contrib import admin
 
-urlpatterns = [path("secret-admin/", admin.site.urls)]
+urlpatterns = [
+    path("secret-admin/", admin.site.urls),
+]
 
 admin.site.index_title = "Crypto Assets"
 admin.site.site_title = "Crypto Assets Django Admin"
