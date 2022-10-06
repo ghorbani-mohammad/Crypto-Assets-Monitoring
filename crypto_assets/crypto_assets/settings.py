f2fa9a56ee33d66489df59a4038807e31905eef7
Mohammad <mohammad.ghorbani@hotmail.com>
2022-10-06 22:30:42 +0330

    minor change

--- crypto_assets/crypto_assets/settings.py
+++ crypto_assets/crypto_assets/settings.py
@@ -75,7 +75,12 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 if DEBUG:
-    STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+    STATICFILES_DIRS = (
+        os.path.join(
+            BASE_DIR,
+            "static",
+        ),
+    )
 else:
     STATIC_ROOT = os.path.join(
         BASE_DIR,
