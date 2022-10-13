ea81f49ab1f157917788fef7d4983ae23b050b9c
Mohammad <mohammad.ghorbani@hotmail.com>
2022-10-13 16:55:04 +0330

    minor change

--- crypto_assets/user/admin.py
+++ crypto_assets/user/admin.py
@@ -5,5 +5,5 @@ from . import models
 
 @admin.register(models.Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    search_fields = ["username"]
+    search_fields = ("username",)
     list_display = ["pk", "first_name", "last_name", "mobile_number", "username"]
