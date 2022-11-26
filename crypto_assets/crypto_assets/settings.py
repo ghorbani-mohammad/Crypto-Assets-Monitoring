ab9b9505a858784f682f97df1852c8327e191778
Mohammad <mohammad.ghorbani@hotmail.com>
2022-11-26 18:16:24 +0330

    minor change

--- crypto_assets/crypto_assets/settings.py
+++ crypto_assets/crypto_assets/settings.py
@@ -89,12 +89,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "static", "media")
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # CELERY
-BROKER_URL = "redis://crypto_assets_redis:6379"
 CELERY_RESULT_BACKEND = "redis://crypto_assets_redis:6379"
 CELERY_ACCEPT_CONTENT = ["application/json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
 CELERY_TIMEZONE = "Asia/Tehran"
+BROKER_URL = "redis://crypto_assets_redis:6379"
 
 DATABASES = {
     "default": {
