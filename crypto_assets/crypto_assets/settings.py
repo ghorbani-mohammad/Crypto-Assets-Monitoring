e2927218f6c39d7ffe382ca862b6b16cd9ef8881
Mohammad <mohammad.ghorbani@hotmail.com>
2022-11-26 18:08:26 +0330

    minor change

--- crypto_assets/crypto_assets/settings.py
+++ crypto_assets/crypto_assets/settings.py
@@ -129,9 +129,9 @@ AUTH_USER_MODEL = "user.Profile"
 # Email Configs
 EMAIL_HOST = "smtp-mail.outlook.com"
 EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 EMAIL_HOST_USER = env("EMAIL_HOST_USER", default=None)
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD", default=None)
-EMAIL_USE_TLS = True
 
 # Email Logging Configs
 ADMIN_EMAIL_LOG = env("ADMIN_EMAIL_LOG", default=None)
