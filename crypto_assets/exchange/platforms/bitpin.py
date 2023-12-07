a9d82d506a547734b03cd021f94cf2fa9619445a
Mohammad <mohammad.ghorbani@hotmail.com>
2023-12-07 23:50:48 +0330

    style(platform): improves code readability

--- crypto_assets/exchange/platforms/bitpin.py
+++ crypto_assets/exchange/platforms/bitpin.py
@@ -1,6 +1,6 @@
 import logging
-import decimal
 import requests
+from decimal import Decimal
 from django.core.cache import cache
 
 from .base import BaseExchange
@@ -13,7 +13,7 @@ class Bitpin(BaseExchange):
     def __init__(self):
         self.api_addr = "https://api.bitpin.ir/v1/mkt/markets/"
 
-    def get_price(self, coin: str, market: str):
+    def get_price(self, coin: str, market: str) -> Decimal:
         market = self.market_mapper(market)
         coin_key = f"{coin}_{market}"
         if cache.get(coin_key):
@@ -23,7 +23,7 @@ class Bitpin(BaseExchange):
             for item in coins:
                 if item["code"] != coin_key:
                     continue
-                price = round(decimal.Decimal(item["price"]), 2)
+                price = round(Decimal(item["price"]), 2)
                 cache.set(coin_key, price, 2)
                 return price
         except Exception as e:
@@ -33,7 +33,7 @@ class Bitpin(BaseExchange):
     def cache_all_prices(self, ttl=2):
         coins = requests.get(self.api_addr).json()["results"]
         for coin in coins:
-            price = round(decimal.Decimal(coin["price"]), 2)
+            price = round(Decimal(coin["price"]), 2)
             cache.set(coin["code"], price, ttl)
 
     def market_mapper(self, market: str):
