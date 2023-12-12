6999e5a67586fa5a4028b1d913b7b4ee1562abaf
Mohammad <mohammad.ghorbani@hotmail.com>
2023-12-12 23:28:19 +0330

    feat(notification): adds NotificationAdmin class

--- crypto_assets/notification/admin.py
+++ crypto_assets/notification/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from reusable.admins import ReadOnlyAdminDateFields
+
+from . import models
+
+
+@admin.register(models.Notification)
+class NotificationAdmin(ReadOnlyAdminDateFields, admin.ModelAdmin):
+    list_display = ("pk", "price", "coin", "profile","status")
