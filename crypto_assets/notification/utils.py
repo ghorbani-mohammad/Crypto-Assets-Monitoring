9dedba318141b35b0aaeed7b1fb13bc5963eaecd
Mohammad <mohammad.ghorbani@hotmail.com>
2023-12-14 08:49:12 +0330

    style: solves linter issues

--- crypto_assets/notification/utils.py
+++ crypto_assets/notification/utils.py
@@ -19,8 +19,8 @@ def send_telegram_message(token: str, chat_id: str, message: str):
 
 def get_coin_cached_prices():
     result = {}
-    redis_client = redis.StrictRedis(host='crypto_assets_redis', port=6379, db=10)
-    coin_keys = redis_client.keys('*coin_*')
+    redis_client = redis.StrictRedis(host="crypto_assets_redis", port=6379, db=10)
+    coin_keys = redis_client.keys("*coin_*")
     for coin_key in coin_keys:
         coin_price = pickle.loads(redis_client.get(coin_key))
         coin_key = coin_key.decode().split("coin_")[-1]
