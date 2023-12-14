d0ca22d66e90139a1ee6e1c55817685904f585d3
Mohammad <mohammad.ghorbani@hotmail.com>
2023-12-14 06:01:51 +0330

    feat(bitpin): improves the code

--- crypto_assets/exchange/platforms/bitpin.py
+++ crypto_assets/exchange/platforms/bitpin.py
@@ -12,6 +12,8 @@ logger = logging.getLogger(__name__)
 class Bitpin(BaseExchange):
     def __init__(self):
         self.api_addr = "https://api.bitpin.ir/v1/mkt/markets/"
+        self.price_round = 5
+        self.cache_price_ttl = 60
 
     def get_price(self, coin, market: str) -> Decimal:
         market = self.market_mapper(market)
@@ -24,18 +26,18 @@ class Bitpin(BaseExchange):
             for item in coins:
                 if item["code"] != coin_key:
                     continue
-                price = round(Decimal(item["price"]), 2)
-                cache.set(coin_key, price, 60)
+                price = round(Decimal(item["price"]), self.price_round)
+                cache.set(coin_key, price, self.cache_price_ttl)
                 return price
         except Exception as e:
             logger.error(e)
             return None
 
-    def cache_all_prices(self, ttl=60):
+    def cache_all_prices(self):
         coins = requests.get(self.api_addr, timeout=10).json()["results"]
         for coin in coins:
-            price = round(Decimal(coin["price"]), 2)
-            cache.set(coin["code"], price, ttl)
+            price = round(Decimal(coin["price"]), self.price_round)
+            cache.set(coin["code"], price, self.cache_price_ttl)
 
     def market_mapper(self, market: str):
         if market == "tether":
--- crypto_assets/exchange/tasks.py
+++ crypto_assets/exchange/tasks.py
@@ -6,4 +6,4 @@ from .platforms.bitpin import Bitpin
 @app.task(name="update_bitpin_prices")
 def update_bitpin_prices():
     bitpin = Bitpin()
-    bitpin.cache_all_prices(ttl=30)
+    bitpin.cache_all_prices()
