947756a267798adc146632c46b00a6b72074867c
Mohammad <mohammad.ghorbani@hotmail.com>
2024-01-01 00:46:29 +0330

    chore(asset): adds value field and AssetAdmin

--- crypto_assets/asset/models.py
+++ crypto_assets/asset/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from user.models import Profile
+from exchange.models import Coin
+from reusable.models import BaseModel
+
+
+class Asset(BaseModel):
+    profile = models.ForeignKey(
+        Profile, related_name="assets", on_delete=models.CASCADE
+    )
+    coin = models.ForeignKey(Coin, related_name="assets", on_delete=models.CASCADE)
+    quantity = models.DecimalField(max_digits=20, decimal_places=10)
+    value = models.DecimalField(max_digits=10, decimal_places=2, null=True)
