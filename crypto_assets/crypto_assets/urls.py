41216c4c52c352a2382e5672dd2e5af7de289c77
Mohammad <mo.ghorb@gmail.com>
2025-04-27 16:13:52 +0330

    chore: update admin site titles for consistency in the Django admin interface

--- crypto_assets/crypto_assets/urls.py
+++ crypto_assets/crypto_assets/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.contrib import admin
 
+
 urlpatterns = [path("secret-admin/", admin.site.urls)]
 
 admin.site.index_title = "Crypto Assets"
-admin.site.site_title = "Crypto Assets Django Admin"
-admin.site.site_header = "Crypto Assets Django Administration Panel"
+admin.site.site_title = "Crypto Assets Admin"
+admin.site.site_header = "Crypto Assets Administration Panel"
